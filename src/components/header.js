42126db2a1490c72132ee439b8f4197b0ef5a75f
kelly lu <klu001@citymail.cuny.edu>
2019-04-06 04:17:52 -0400

    create component header

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,33 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
+// import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+class Header extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  // NEED TO OBTAIN PROPS FROM PARENT TO IDENTIFY WHETHER TO SHOW SIGN IN OR LOG OUT
+  render () {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+          {/* add <a className="navbar-brand"><img src="../.."></a>* when we have a logo */}
+          <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#collapse_target" aria-controls="collapse_target" aria-expanded="false" aria-label="Toggle naviagation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <span className="navbar-text">Universal Design Compass</span>
+          <div className="collapse navbar-collapse" id="collapse_target">
+            <ul className="navbar-nav">
+              <li className="nav-item"><Link to="/" className="nav-link">Home</Link></li>
+              <li className="nav-item"><Link to="/" className="nav-link">About Us</Link></li>
+              <li className="nav-item"><Link to="/" className="nav-link">Contact</Link></li>
+              <li className="nav-item"><Link to="/" className="nav-link">Create</Link></li>
+              <li className="nav-item"><Link to="/" className="nav-link">LogIn</Link></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
+  }
 }
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header
+export default Header;
