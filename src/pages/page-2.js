100372d8a0b9333a7ee81b98d2034afcdc00b9b4
rdiaz002 <diaz.r93@gmail.com>
2019-04-06 01:15:47 -0400

    fixed amplify auth login screen and redirecting to home

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,13 +1,13 @@
 import React from "react"
 import {SignIn} from "aws-amplify-react";
 import config from "../aws-exports";
-import { CustomSignIn } from "./CustomSignIn";
+import Login from "./CustomSignIn";
 import {Authenticator} from "aws-amplify-react/dist/Auth"
 class sec extends React.Component{
   render(){
     return(
       <Authenticator hide={[SignIn]} amplifyConfig={config}>
-        <CustomSignIn/>
+        <Login/>
       </Authenticator>
     )
   }
