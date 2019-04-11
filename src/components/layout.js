dbfe38e6bdbfccf1f3b8fa5c4f3b0ffdda2824f9
gutierrezjeter <gutierrezjeter@gmail.com>
2019-04-10 21:15:32 -0400

    create footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-
+import Footer from './footer';
 import Header from "./header"
 import "./layout.css"
 
@@ -27,6 +27,7 @@ const Layout = ({ children }) => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
         <main>{children}</main>
+        <Footer />
       </>
     )}
   />
