e0e8ba36f8723f510225310d0c34872e0416c027
kelly lu <klu001@citymail.cuny.edu>
2019-04-15 12:40:22 -0400

    conditional login / logout profile options

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,16 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 import { Navbar, Nav } from 'react-bootstrap';
-import NavBarStyles from '../styles/components.modules.css'
+import NavBarStyles from '../styles/components.modules.css';
+import { store } from '../state/store';
+
+var userStatus = store.getState().Reducer.isAuthenticated;
+var displayLogin = (userStatus == "false") ?
+  ( <Nav.Link><Link rel="login" to="/Login">Login</Link></Nav.Link> ) :
+  ( <Nav.Link><Link rel="logout" to="/">Logout</Link></Nav.Link> );
+
+var displayProfile = (userStatus == "false") ?
+  null : ( <Nav.Link><Link rel="profile" to="/">Profile</Link></Nav.Link> );
 
 const Header = (props) => (
   <Navbar collapseOnSelect sticky="top" expand="md" bg="dark" variant="dark">
@@ -14,12 +23,12 @@ const Header = (props) => (
         <Nav.Link><Link rel="about" to="/About">About Us</Link></Nav.Link>
         <Nav.Link><Link rel="create" to="/">Create</Link></Nav.Link>
         <Nav.Link><Link rel="compass" to="/Compass">Compass</Link></Nav.Link>
-        {/* The two below requires a props.status (loged in or out?) to determine what link to display */}
-        <Nav.Link><Link rel="profile" to="/">Profile</Link></Nav.Link>
-        <Nav.Link><Link rel="login" to="/Login">Login</Link></Nav.Link>
+        {displayProfile}
+        {displayLogin}
       </Nav>
     </Navbar.Collapse>
   </Navbar>
+
 );
 
 
