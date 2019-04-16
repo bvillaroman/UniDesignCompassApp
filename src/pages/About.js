43f973ae95692a02b07e7823fa2343f639f08411
kelly lu <klu001@citymail.cuny.edu>
2019-04-16 15:31:20 -0400

    create profile.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,6 +7,7 @@ class About extends React.Component {
         <Layout>
           <h1 className="text-center">Our Mission</h1>
           <h1 className="text-center">The Team</h1>
+          <h1 className="text-center">Contact</h1>
         </Layout>
       );
   }
---
+++ src/pages/Profile.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import { store } from '../state/store';
+
+class Profile extends React.Component {
+//store.getState().Render.aws_user
+}
