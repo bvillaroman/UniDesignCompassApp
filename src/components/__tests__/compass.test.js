8277f681f8f4ad9b4fbce3259eef9d6fd2f31f7a
rdiaz002 <rdiaz002@citymail.cuny.edu>
2019-04-18 13:53:56 -0400

    Added testing files

---
+++ src/components/__tests__/compass.test.js
@@ -0,0 +1,36 @@
+import React from "react"
+import renderer from "react-test-renderer"
+import Login from "../../pages/Login";
+import { store } from "../../state/store";
+import { Provider } from 'react-redux';
+import {configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import {mount} from 'enzyme';
+import {mountToJson} from 'enzyme-to-json';
+configure({adapter:new Adapter()});
+
+const tree = mount(<Provider store={store} ><Login /></Provider>);
+
+describe("Check Timer", () => {
+    it("render", () => {
+        expect(mountToJson(tree)).toMatchSnapshot()
+    })
+})
+describe("Check Log", () => {
+    it("render", () => {
+        expect(mountToJson(tree)).toMatchSnapshot()
+    })
+})
+describe("Check Compass Page", () => {
+    it("render", () => {
+        expect(mountToJson(tree)).toMatchSnapshot()
+    })
+})
+
+
+// describe("SignIn exceptions",() =>{
+//      it("failed Log in",()=>{
+//          console.log();
+//         tree.find('[type="submit"]');
+//      })
+// })
