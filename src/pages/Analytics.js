fc69b9209cc51d288178e46eb5ecb62b98c51698
Ramon Petgrave <ramon.petgrave64@gmail.com>
2019-04-18 18:00:13 -0400

    better to way to use vega, with an independent BarChart element

--- src/pages/Analytics.js
+++ src/pages/Analytics.js
@@ -4,47 +4,49 @@ import Vega from 'react-vega';
 
 import Layout from "../components/layout"
 import { Button } from "react-bootstrap";
+import BarChart from "./BarChart";
 
 class Analytics extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            spec: {
-                "width": 400,
-                "height": 200,
-                "data": [{ "name": "table" }],
-                "signals": [
-                  {
-                    "name": "tooltip",
-                    "value": {},
-                    "on": [
-                      {"events": "rect:mouseover", "update": "datum"},
-                      {"events": "rect:mouseout",  "update": "{}"}
-                    ]
-                  }
-                ],
-                 // See the rest in demo/src/vega/spec1.js
-            },
-            barData: {
-                table: [{"x": 1,  "y": 28}, {"x": 2,  "y": 55}]
-            }
-        }
     }
-      
+    
     handleHover(...args){
     console.log(args);
     }
         
     render() {
+        console.log(this.state);
         return (
             <Layout>
                 <div className='container'>
-                    <Vega spec={this.state.spec} data={this.state.barData} />
-                    <Button>ok</Button>
+                    {/* <Vega spec={this.state.spec} data={this.state.data} /> */}
+                    <BarChart data={this.data} />
+                    <Button onClick={this.chartButtonHandler}>ok</Button>
                 </div>
             </Layout>
         );
     }
+
+    chartButtonHandler = () => {
+        console.log("handlinng");
+    }
+
+    data = [
+        {
+          "name": "table",
+          "values": [
+            {"category": "A", "amount": 28},
+            {"category": "B", "amount": 55},
+            {"category": "C", "amount": 43},
+            {"category": "D", "amount": 91},
+            {"category": "E", "amount": 81},
+            {"category": "F", "amount": 53},
+            {"category": "G", "amount": 19},
+            {"category": "H", "amount": 87}
+          ]
+        }
+    ]
 }
 
 export default Analytics;
