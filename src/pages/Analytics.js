e2c03b41ad327a72ab17c329b7729a6b2b97a17a
kelly lu <klu001@citymail.cuny.edu>
2019-04-20 19:55:06 -0400

    merge analysis page and analytics page

--- src/pages/Analytics.js
+++ src/pages/Analytics.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-
-import Layout from "../components/layout"
+import { Table } from 'react-bootstrap';
+import Layout from "../components/layout";
 import ResponsiveBarChart from '../components/ResponsiveBarChart';
 
 class Analytics extends Component {
@@ -19,13 +19,64 @@ class Analytics extends Component {
     onSize = size => {
         console.log('MyComponent has a width of', size.width)
       }
-        
+
     render() {
         return (
             <Layout>
                 <div className='container'>
                     <ResponsiveBarChart data={this.data}/>
                 </div>
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>Stage</th>
+                      <th>Notes</th>
+                      <th>Time</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Define Problem</td>
+                      <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas integer eget aliquet nibh. Cras pulvinar mattis nunc sed blandit libero. Commodo odio aenean sed adipiscing. Lorem ipsum dolor sit amet consectetur adipiscing elit.</td>
+                      <td>12:00:00</td>
+                    </tr>
+                    <tr>
+                      <td>Research</td>
+                      <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas integer eget aliquet nibh. Cras pulvinar mattis nunc sed blandit libero. Commodo odio aenean sed adipiscing. Lorem ipsum dolor sit amet consectetur adipiscing elit.</td>
+                      <td>12:00:00</td>
+                    </tr>
+                    <tr>
+                      <td>Brainstorm</td>
+                      <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas integer eget aliquet nibh. Cras pulvinar mattis nunc sed blandit libero. Commodo odio aenean sed adipiscing. Lorem ipsum dolor sit amet consectetur adipiscing elit.</td>
+                      <td>12:00:00</td>
+                    </tr>
+                    <tr>
+                      <td>Select</td>
+                      <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas integer eget aliquet nibh. Cras pulvinar mattis nunc sed blandit libero. Commodo odio aenean sed adipiscing. Lorem ipsum dolor sit amet consectetur adipiscing elit.</td>
+                      <td>12:00:00</td>
+                    </tr>
+                    <tr>
+                      <td>Construct</td>
+                      <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas integer eget aliquet nibh. Cras pulvinar mattis nunc sed blandit libero. Commodo odio aenean sed adipiscing. Lorem ipsum dolor sit amet consectetur adipiscing elit.</td>
+                      <td>12:00:00</td>
+                    </tr>
+                    <tr>
+                      <td>Evaluate</td>
+                      <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas integer eget aliquet nibh. Cras pulvinar mattis nunc sed blandit libero. Commodo odio aenean sed adipiscing. Lorem ipsum dolor sit amet consectetur adipiscing elit.</td>
+                      <td>12:00:</td>
+                    </tr>
+                    <tr>
+                      <td>Communicate</td>
+                      <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas integer eget aliquet nibh. Cras pulvinar mattis nunc sed blandit libero. Commodo odio aenean sed adipiscing. Lorem ipsum dolor sit amet consectetur adipiscing elit.</td>
+                      <td>12:00:</td>
+                    </tr>
+                    <tr>
+                      <td>Redesign</td>
+                      <td>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et malesuada fames ac turpis egestas integer eget aliquet nibh. Cras pulvinar mattis nunc sed blandit libero. Commodo odio aenean sed adipiscing. Lorem ipsum dolor sit amet consectetur adipiscing elit.</td>
+                      <td>12:00:00</td>
+                    </tr>
+                  </tbody>
+                </Table>
             </Layout>
         );
     }
