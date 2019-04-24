03f41abc6b07c9260a835efa882f830566c97b08
Ramon Petgrave <ramon.petgrave64@gmail.com>
2019-04-24 01:06:30 -0400

    add bootstrap css to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import HomePage from "./HomePage"
 import { Provider } from 'react-redux';
 import { store } from "../state/store";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const IndexPage = (props) => (
   <Provider store={store}>
