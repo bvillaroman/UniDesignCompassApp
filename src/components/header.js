c7faa2983e855d3e49b351e129ce3a8af090feab
Ramon Petgrave <ramon.petgrave64@gmail.com>
2019-04-24 16:53:36 -0400

    fixed styling with new Links, added underlining

--- src/components/header.js
+++ src/components/header.js
@@ -7,31 +7,41 @@ import { logOutUser } from '../state/actions';
 
 const signedIn=(props)=>{
   if(props.isAuthenticated){
-    return(<NavItem><Link rel="profile" to="/Profile">Profile</Link></NavItem>)
+    return(<NavItem><Link style={styles.link} activeStyle={styles.linkActive}  rel="profile" to="/Profile">Profile</Link></NavItem>)
   }
 }
 
 const Header = (props) => {
   return (
     <Navbar collapseOnSelect sticky="top" expand="md" bg="dark" variant="dark">
-      <Navbar.Brand variant="light"> <Link to="/"> {props.siteTitle}</Link> </Navbar.Brand>
+      <Navbar.Brand variant="light"> <Link to="/" style={styles.link}> {props.siteTitle}</Link> </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav>
-          <NavItem><Link rel="about" to="/About">About Us</Link></NavItem>
-          <NavItem><Link rel="compass" to="/Compass">Compass</Link></NavItem>
-          <NavItem><Link rel="analytics" to="/Analytics">Analytics</Link></NavItem>
+          <NavItem><Link style={styles.link} activeStyle={styles.linkActive} rel="about" to="/About">About Us</Link></NavItem>
+          <NavItem><Link style={styles.link} activeStyle={styles.linkActive} rel="compass" to="/Compass">Compass</Link></NavItem>
+          <NavItem><Link style={styles.link} activeStyle={styles.linkActive} rel="analytics" to="/Analytics">Analytics</Link></NavItem>
           {signedIn(props)}
           {
             props.isAuthenticated ?
-              (<NavItem><Link rel="logout" to="/" onClick={() => { props.logOutUser(); navigate("/"); }}>Logout</Link></NavItem>)
-              : (<NavItem><Link rel="login" to="/Login">Login</Link></NavItem>)
+              (<NavItem><Link style={styles.link} rel="logout" to="/" onClick={() => { props.logOutUser(); navigate("/"); }}>Logout</Link></NavItem>)
+              : (<NavItem><Link style={styles.link} activeStyle={styles.linkActive} rel="login" to="/Login">Login</Link></NavItem>)
           }
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   )
 };
+
+const styles = {
+  link: {
+    color: 'white',
+    textDecoration: 'none'
+  },
+  linkActive: {
+    textDecoration: 'underline'
+  }
+}
 
 Header.propsTypes = {
   isAuthenticated: PropTypes.bool,
