d1f506c4e8d14ef79fbf3a92d17d75a7cd56f3ea
Bruce <bvillaroman@gmail.com>
2019-04-30 10:22:33 -0400

    added the ability to create custom phases, where createCompass now returns an updated user object with new phases

--- src/graphql_utils/utils.js
+++ src/graphql_utils/utils.js
@@ -13,7 +13,6 @@ export async function getUser(id){
 export async function getUserbyUsername(username){
     const filter = { username : { eq : username } }
     const user = await API.graphql(graphqlOperation(queries.listUsers,{filter}));
-    console.log(user);
     const userinfo = await getUser(user.data.listUsers.items[0].id);
     return userinfo;
 }
@@ -70,7 +69,6 @@ async function createPhase(processId,title,description){
         description:description,
         duration:"0000000",
         phaseProcessId: processId
-
     }
     const newPhase = await API.graphql(graphqlOperation(mutations.createPhase,{input:phaseInfo}));
     return newPhase;
@@ -141,22 +139,16 @@ export async function createNewCompass(user,name,phases){
     await createProcess(user.id,name,"0000000","00000000").then(
         (res)=>{
             process_info=res.data.createProcess;
-            console.log(process_info);
         },(error)=>{
             console.log(error);
         }
     )
-    console.log("Hello");
     
-    for(let i =0 ;i < phases.length; i++){
-        await createPhase(process_info.id,phases[i].title,phases[i].description);
-    }
+    for(let i =0 ;i < phases.length; i++){ await createPhase(process_info.id,phases[i].title,phases[i].description);}
     
     await getUser(user.id).then(
         (res)=>{
-            console.log(user)
-            user=res.data.getUser;
-            console.log(user)
+            return process_info;
         },(error)=>{
             console.log(error);
         }
