aa6818348e4658175cf0b5f45ea4025997f9bdc8
Ramon Petgrave <ramon.petgrave64@gmail.com>
2019-05-07 14:39:29 -0400

    dont use store props in state for LogCard

--- src/components/LogCard.js
+++ src/components/LogCard.js
@@ -5,7 +5,7 @@ class LogCard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            logData: this.props.logData,
+            // logData: this.props.logData,
             deleteHandler: this.props.deleteHandler,
             updateHandler: this.props.updateHandler,
             editText: null,
@@ -13,21 +13,21 @@ class LogCard extends Component {
         }
     }
 
-    componentDidUpdate(prevProps) {
-        const p = ['id', 'phase_title', 'text', 'timestamp'];
-        const log_is_updated = p.some(item => {
-            return prevProps.logData[item] !== this.state.logData[item];
-        });
-        if(log_is_updated)
-            this.setState({logData: this.props.logData})
-    }
+    // componentDidUpdate(prevProps) {
+    //     const p = ['id', 'phase_title', 'text', 'timestamp'];
+    //     const log_is_updated = p.some(item => {
+    //         return prevProps.logData[item] !== this.state.logData[item];
+    //     });
+    //     if(log_is_updated)
+    //         this.setState({logData: this.props.logData})
+    // }
 
     edit_toggle_handler = () => {
         this.setState({editing: !this.state.editing})
     }
 
     update_button_handler = () => {
-        this.state.updateHandler(this.state.logData.id, Date.now(), this.state.editText);
+        this.state.updateHandler(this.props.logData.id, Date.now(), this.state.editText);
         this.setState({editing: false})
     }
 
@@ -36,7 +36,7 @@ class LogCard extends Component {
     }
         
     render() {
-        // console.log(this.state.logData)
+        // console.log(this.props.logData)
         return (
             <div className={'card mb-3 shadow-sm'}>
                 <div className={'card-header'}>
@@ -44,10 +44,10 @@ class LogCard extends Component {
                         <div className={'col-lg-8 col-sm-12'}>
                             <span className={'row'}>
                                 <span className={'col-lg-6 col-4'}>
-                                    {this.state.logData.phase_title}
+                                    {this.props.logData.phase_title}
                                 </span>
                                 <span className={'col-lg-6 col-8 text-lg-center text-right'}>
-                                    {new Date(parseInt(this.state.logData.timestamp)).toLocaleString()}
+                                    {new Date(parseInt(this.props.logData.timestamp)).toLocaleString()}
                                 </span>
                             </span>
                         </div>
@@ -75,7 +75,7 @@ class LogCard extends Component {
                                         </button>
                                         <button
                                             className={'btn mr-lg-0 ml-lg-1 mr-3 ml-3 btn-outline-danger col-lg-3'}
-                                            onClick={() => this.state.deleteHandler(this.state.logData.id)}
+                                            onClick={() => this.state.deleteHandler(this.props.logData.id)}
                                         >Delete
                                         </button>
                                     </span>
@@ -85,13 +85,13 @@ class LogCard extends Component {
                 </div>
                 <div className={'card-body'}>
                     {/* <h5 class="card-title">Special title treatment</h5> */}
-                    <p className={'card-text'}>{this.state.logData.text}</p>
+                    <p className={'card-text'}>{this.props.logData.text}</p>
                     {this.state.editing
                         ? 
                             <textarea 
                                 className={'form-control'} 
                                 rows={3} 
-                                defaultValue={this.state.logData.text} 
+                                defaultValue={this.props.logData.text} 
                                 onChange={this.textarea_handler}
                             />
                         : 
