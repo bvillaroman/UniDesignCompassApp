f45871f3b324db862d433589e7c82c7d60bef907
rdiaz002 <rdiaz002@citymail.cuny.edu>
2019-05-07 14:25:10 -0400

    Changed the Dummy data for a more professional compass.

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,16 +1,16 @@
 export const log_list ={
 data:[
     {id:'1',
-    text:"Hello MY lady Hello My lovely Hello MY Rag TIme Gal"},
+    text:"Where is a chair folded?"},
     {id:'1',
-    text:"Hello MY lady Hello My lovely Hello MY Rag TIme Gal"},
+    text:"Do chairs need folding?"},
     {id:'2',
-    text:"Hello MY lady Hello My lovely Hello MY Rag TIme Gal"},
+    text:"Found a list of all possible chairs"},
     {id:'4',
-    text:"Hello MY lady Hello My lovely Hello MY Rag TIme Gal"},
+    text:"Selected My chair material"},
     {id:'1',
-    text:"Hello MY lady Hello My lovely Hello MY Rag TIme Gal"},
+    text:"How do we fold a chair in half?"},
     {id:'2',
-    text:"Hello MY lady Hello My lovely Hello MY Rag TIme Gal"}
+    text:"Found Folding Patterns"}
     ]
 }
