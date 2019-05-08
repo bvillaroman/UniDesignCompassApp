d2e4858c6d4759919505b829d1817a7128391282
Bruce <bvillaroman@gmail.com>
2019-05-07 23:32:07 -0400

    loaded the dynamic user data into Process component

--- src/pages/Process.js
+++ src/pages/Process.js
@@ -1,29 +1,45 @@
 import React from "react";
 import { Tab, Tabs } from 'react-bootstrap';
 import Layout from "../components/layout";
+import {getProcess} from "../graphql_utils/utils" 
 
 class Profile extends React.Component {
 
-  state = {
-
+  state = {        
+    date_end: "",
+    date_start: "",
+    id: "",
+    name: "",
+    phases: [],
   }
 
   componentDidMount() {
+    const { id } = this.props.pageContext;
+    getProcess(id)
+    .then((res) => {
+      const {date_end, date_start, id, name, phaseids : { items }} = res.data.getProcess
 
+      this.setState({
+        date_end, 
+        date_start, 
+        id, 
+        name, 
+        phases: items
+      })
+    })
   }
-
 
   render() {
 
     return (
       <Layout>
-        <Tabs defaultActiveKey="projects" transitions={true} style={{width:100 + "%"}}>
-          <Tab eventKey="projects" title="Projects">
-            <h2 className="text-center">Compass</h2>
-          
+        <Tabs defaultActiveKey="projects" transitions="true" style={{width:100 + "%"}}>
+          <Tab eventKey="projects" title="Compass">
+            <h2 className="text-center">{this.state.name}</h2>
+
           </Tab>
           <Tab eventKey="settings" title="Analytics">
-            <h2 className="text-center">Analytics</h2>
+            <h2 className="text-center">{this.state.name}</h2>
             
           </Tab>
         </Tabs>
