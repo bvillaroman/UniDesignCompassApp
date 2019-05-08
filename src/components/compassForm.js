d7523a9811732f89a34a88a1ebb7ecda7144a514
Bruce <bvillaroman@gmail.com>
2019-05-08 02:20:40 -0400

    added styling to create compass page

--- src/components/compassForm.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Card, } from 'react-bootstrap';
-
-const CompassForm = (props) => (
-  <Card>
-      {props.children}
-  </Card>
-);
-export default CompassForm;
