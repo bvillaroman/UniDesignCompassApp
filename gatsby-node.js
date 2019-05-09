90a35fffb1bc03b8d9d66d601af265d534f84e2b
Bruce <bvillaroman@gmail.com>
2019-05-09 00:12:36 -0400

    removed faq page for build error

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,24 +5,61 @@
  */
 
 // You can delete this file if you're not using it
-exports.createPages = async ({actions, graphql}) => {
-  const { data } = await graphql(`
-    query {
-      events {
-        listProcesss (limit: 1000){
-          items {
-            id
+// exports.createPages = async ({actions, graphql}) => {
+//   const { data } = await graphql(`
+//     query {
+//       events {
+//         listProcesss (limit: 1000){
+//           items {
+//             id
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   data.events.listProcesss.items.forEach(process => {
+//     const id = process.id
+//     actions.createPage({
+//       path: `Process/${id}`,
+//       component: require.resolve(`./src/pages/Process.js`),
+//       context: { id: id },
+//     })
+//   })
+// }
+
+// Implement the Gatsby API “createPages”. This is called once the
+// data layer is bootstrapped to let plugins create pages from data.
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return new Promise((resolve, reject) => {
+    // Query for markdown nodes to use in creating pages.
+    resolve(
+      graphql(
+        `
+        query {
+          events {
+            listProcesss (limit: 1000){
+              items {
+                id
+              }
+            }
           }
         }
-      }
-    }
-  `)
-  data.events.listProcesss.items.forEach(process => {
-    const id = process.id
-    actions.createPage({
-      path: `Process/${id}`,
-      component: require.resolve(`./src/pages/Process.js`),
-      context: { id: id },
-    })
+        `
+      ).then(result => {
+        if (result.errors) {
+          reject(result.errors)
+        }
+
+        result.data.events.listProcesss.items.forEach(process => {
+          const id = process.id
+          createPage({
+            path: `Process/${id}`,
+            component: require.resolve(`./src/pages/Process.js`),
+            context: { id: id },
+          })
+        })
+      })
+    )
   })
 }
