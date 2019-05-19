1d68e744339f1005c815d27cb4d95c00a4927707
rdiaz002 <rdiaz002@citymail.cuny.edu>
2019-05-19 17:59:03 -0400

    modified login page test

--- src/components/__tests__/login.test.js
+++ src/components/__tests__/login.test.js
@@ -1,46 +1,27 @@
 import React from "react"
-import renderer from "react-test-renderer"
-import Login from "../../pages/Login";
-import { store } from "../../state/store";
-import { Provider } from 'react-redux';
-import {configure} from 'enzyme';
+import { Login } from "../../pages/Login";
+import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import {shallow} from 'enzyme';
-import {shallowToJson} from 'enzyme-to-json';
-configure({adapter:new Adapter()});
+import { shallow } from 'enzyme';
+configure({ adapter: new Adapter() });
 
-const tree = shallow(<Login store={store} />).dive().dive();
+describe("Login page tests", () => {
+    const tree = shallow(<Login />)
+    let state
+    beforeEach(() => {
 
-describe("Check Login_Page", () => {
-    it("render", () => {
-        console.log(tree.instance());
-        expect(shallowToJson(tree)).toMatchSnapshot()
-    })
-})
-describe("Check Password", () => {
-    it("render", () => {
-        expect(shallowToJson(tree)).toMatchSnapshot()
-    })
-})
-describe("Check verification page loads", () => {
-    it("render", () => {
-        expect(shallowToJson(tree)).toMatchSnapshot()
-    })
-})
-describe("Check user signup page ", () => {
-    it("render", () => {
-        expect(shallowToJson(tree)).toMatchSnapshot()
-    })
-})
-describe("", () => {
-    it("render", () => {
-        expect(shallowToJson(tree)).toMatchSnapshot()
+        state = {
+            password: "",
+            username: "",
+            user: [],
+            loading: false
+        };
+
+    });
+    describe("state tests", () => {
+        it("Check Page with Blank State", () => {
+            expect(tree.state()).toEqual(state)
+        })
     })
 })
 
-// describe("SignIn exceptions",() =>{
-//      it("failed Log in",()=>{
-//          console.log();
-//         tree.find('[type="submit"]');
-//      })
-// })
