de6f1aaa8d7905636e2d263b7ccc3ab710d6c310
gutierrezjeter <gutierrezjeter@gmail.com>
2019-05-19 16:48:27 -0400

    Check phase state

--- src/components/__tests__/phase.test.js
+++ src/components/__tests__/phase.test.js
@@ -2,13 +2,25 @@
 import React from 'react'
 import Phase from '../Compass/Phase';
 import LogHandler from '../Compass/LogHandler'
+import {shallow} from 'enzyme'
+import {configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({adapter:new Adapter()});
+
+const Phase1= shallow(<Phase/>)
 jest.mock('../Compass/Phase')
+
 // beforeEach(() => {
   // Clear all instances and calls to constructor and all methods:
   // Phase.mockClear();
 // });
 
-it('If the constructor for the phaseclass is called once we pass.', () => {
-  const Phase1 = new Phase();//Checks if the constructor class for the Phase is called properly.
-  expect(Phase).toHaveBeenCalledTimes(1);
+it('Constructor called properly.', () => {
+  const Phase2= new Phase();//Checks if the constructor class for the Phase is called properly.
+  expect(Phase).toHaveBeenCalledTimes(2);
 });
+
+it('State Set Properly', () => {
+  console.log(Phase1.state())
+});
+
