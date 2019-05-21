bc860f4937a9a94506cae17d2a77cac5dedacbc5
KLU01 <31288854+KLU01@users.noreply.github.com>
2019-05-21 16:38:38 -0400

    Update header.test.js

--- src/components/__tests__/header.test.js
+++ src/components/__tests__/header.test.js
@@ -12,28 +12,7 @@ configure({adapter:new Adapter()});
 const tree = shallow(<Header siteTitle="Universal Compass Design"/>);
 
 describe("Header", () => {
-  // it("renders correctly", () => {
-  //   const tree = renderer
-  //     .create(<Header siteTitle="Universal Design Compass" />)
-  //     .toJSON()
-  //   expect(tree).toMatchSnapshot();
-  // });
   it("Check Render",()=>{
       expect(shallowToJson(tree)).toMatchSnapshot();
   });
-  // it("select about", () => {
-    // const { getByText, getByTestId, container } = render(<Header siteTitle="Universal Design Compass" />);
-    // fireEvent.click(getByText("About Us"));
-    // expect(container.firstchild).toMatchSnapshot();
-    // const { getByText } = render(<Header siteTitle="Universal Design Compass" />);
-    // const aboutButton = getByText("About Us");
-    // const textAbout = aboutButton.textContent;
-    // expect(textAbout).toBe("About Us");
-    // aboutButton.click();
-  // });
-  // it("Check Navbar brand link",()=>{
-  //     // expect(tree.find('#brand-link').text()).toEqual('Universal Compass Design');
-  //     tree.find('Link#brand-link').simulate('click');
-  //     expect('/').toHaveBeenCalled();
-  // });
 });
