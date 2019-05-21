5498e025918b78364b80441662d7aff39f5975e4
gutierrezjeter <gutierrezjeter@gmail.com>
2019-05-21 08:31:17 -0400

    Phase tests

--- src/components/__tests__/phase.test.js
+++ src/components/__tests__/phase.test.js
@@ -34,11 +34,13 @@ it('TimeHandler clean',() =>{
 // it('TimeHandler test',()=>{
 //   let spy=jest.spyOn(phaseWrapper.instance(),"timeHandler")
 //   function compassButtonHandler (x) {//handle current phase too.
-//     this.setState({ currentPhase: x.key })}// 
+//     this.setState({ currentPhase: x.key })}//
+//     // cont 
 // // let compassButtonHandler=jest.spyOn(CompassWrapper.instance(),"compassButtonHandler")
 //   expect(spy).toHaveBeenCalledTimes(0);
 //   let phase={ key: '1', name: 'A. Define Problem', icon: '', description: '', link: '#', time: 0 }
-//   spy(compassButtonHandler,phase)
+//   let props={phase,compassButtonHandler:jest.fn()}
+//   spy(props)
 //   expect(spy).toHaveBeenCalledTimes(0);
 // });
 
