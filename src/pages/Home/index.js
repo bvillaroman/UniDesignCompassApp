504816c7fbd13753691041a2fae6237ca3dd4922
abraham villaroman <bvillaroman@gmail.com>
2019-08-19 19:37:52 -0400

    implemented new directory structure: pages have their own pages to easily preserve page-exclusive components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import Layout from '../components/Layout';
-import Logger from "../components/Logger"
+import Layout from '../../components/Layout';
+import Logger from "../../components/Logger"
 
 const HomePage = (props) => (
   <Layout>
