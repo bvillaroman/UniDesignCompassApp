af50d3a2787ad2ac75fb964d14ae2e29a37778e3
abraham villaroman <bvillaroman@gmail.com>
2019-09-04 09:56:16 -0400

    added clock to sidebar session

--- src/components/CompassComponents/SessionSideBar/index.js
+++ src/components/CompassComponents/SessionSideBar/index.js
@@ -1,12 +1,28 @@
 import React from "react";
+import {Clock} from "grommet"
 import { SessionView } from "../../../styles/CompassPage"
 import {userCompassPage} from "../../../context/CompassPage/context"
 
 const SessionSideBar = (props) => {
-  const { currentSession } = userCompassPage();
+  const { currentSession, currentStep, interaction } = userCompassPage();
     return (
       <SessionView gridArea="session">
         {currentSession.title}
+        <Clock type="digital" />
+        {
+          currentStep.title ? (
+            <>
+              {currentStep.title}
+              <Clock type="digital" />
+              <br/>
+              <br/>  
+              {currentStep.description}
+              <br/>
+              <br/>
+              <p>Attachments</p>
+            </>
+          ) : ''
+        }
       </SessionView>
   )
 }
