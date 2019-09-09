ef515175cbb78b51d44bea2545b179b72049e616
Kenny Tan <kennytanzxc1@gmail.com>
2019-09-09 10:27:48 -0400

    createSession

--- src/components/CompassComponents/SessionCreator/index.js
+++ src/components/CompassComponents/SessionCreator/index.js
@@ -1,43 +1,52 @@
 import React, { useState } from 'react'
-import { userCompassPage } from "../../../context/CompassPage/context"
+//import { userCompassPage } from "../../../context/CompassPage/context"
+import { createSession } from "../../../utils/mutations"
 
 import {
   SCButtonContainer,
   SCButton,
   SCFormContainer,
 } from "../../../styles/CompassPage"
-import { 
-  FormCard, 
-  FormTitle, 
-  InputContainer,  
-  InputField, 
-  InputTextArea 
+import {
+  FormCard,
+  FormTitle,
+  InputContainer,
+  InputField,
+  InputTextArea
 } from "../../../styles/Form"
 
-const SessionCreator = (props) => {  
-  const [form,setForm] = useState({ title: '', description: '' });
-  const [error,setError] = useState({ title: '', description: ''})
-  const {createSession} = userCompassPage();
+const SessionCreator = (props) => {
+  const [form, setForm] = useState({ title: '', description: '' });
+  const [error] = useState({ title: '', description: '' })
+  // const { createSession } = userCompassPage();
 
-  const onChange = ({ target: { value,name } }) => { setForm({...form, [name]: value})  };
+  const onChange = ({ target: { value, name } }) => { setForm({ ...form, [name]: value }) };
+
+  const currentCompassId = localStorage.getItem("compass")
 
   const sendForm = (e) => {
-    createSession(form)
+    createSession(form.title, form.description, currentCompassId)
+      .then((result) => {
+        localStorage.setItem('session', result.data.createSession.id)
+        // 
+
+      })
+      .catch(err => console.log(err))
   }
 
-  return (    
+  return (
     <FormCard alignSelf="center" elevation="medium">
       <FormTitle> Create a Session </FormTitle>
       <SCFormContainer
         onSubmit={sendForm}
         onChange={onChange}
-        errors={{...error}}
+        errors={{ ...error }}
         value={form}
       >
-        <InputContainer  name="title"  required>
+        <InputContainer name="title" required>
           <InputField name="title" type="text" placeholder="Session Title" />
         </InputContainer>
-        <InputContainer  name="description" >
+        <InputContainer name="description" >
           <InputTextArea name="description" placeholder="Session Description" />
         </InputContainer>
         <SCButtonContainer>
