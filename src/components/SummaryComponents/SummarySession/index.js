cacdafdf55fd5d04fc9a0baff2695973ddaa6173
Kenny Tan <kennytanzxc1@gmail.com>
2019-10-03 08:52:40 -0400

    sory logs by time && get compassID by URL

--- src/components/SummaryComponents/SummarySession/index.js
+++ src/components/SummaryComponents/SummarySession/index.js
@@ -3,7 +3,7 @@ import { getCompass, getInteraction } from "../../../utils/queries";
 import { GlobalContext } from '../../../context/context';
 import SummaryLog from '../SummaryLog/';
 import SummaryLegend from '../SummaryLegend/';
-import CompassBar from '../../SideBarComponents/CompassBar'
+// import CompassBar from '../../SideBarComponents/CompassBar'
 import { Image } from 'grommet-icons';
 import {
   SummaryMainView,
@@ -16,13 +16,15 @@ import {
   SummaryTableConatiner
 } from '../../../styles/SummaryPage';
 
-const SummarySession = () => {
+const SummarySession = (props) => {
   const { compass } = useContext(GlobalContext)
   const [sessions, setSession] = useState([])
   // const [compassID, setCompassID] = useState('')
   const [expandLog, setExpandLog] = useState("")
   const [render, showRender] = useState(false)
 
+  const compassID = props.compassID.slice(3);
+  console.log()
   //Mounting once when the page loads
   useEffect(() => {
     getCompass(compass)
@@ -43,11 +45,21 @@ const SummarySession = () => {
       .catch(err => console.log('getSessionLogs err:', err))
   }
 
+  function timeConverter(a, b) {
+    if (a.createdAt > b.createdAt) {
+      return -1
+    } else if (a.createdAt < b.createdAt) {
+      return 1
+    } else {
+      return 0
+    }
+  }
+
   const SessionTable = ({ sessions }) => {
     return (
       <>
-        <SummaryMainView>{sessions.map(session =>
-          <SummaryTableConatiner style={{ margin: '1em', width: '96%' }}>
+        <SummaryMainView>{sessions.sort(timeConverter).map((session, i) =>
+          <SummaryTableConatiner style={{ margin: '1em', width: '96%' }} key={i}>
             <div style={{ fontSize: 'x-large', fontWeight: 500 }}>
               {session.name_of_session}
             </div>
@@ -56,15 +68,18 @@ const SummarySession = () => {
                 <SummaryTableRow style={{ fontSize: "initial" }}>
                   <SummaryTdHeader style={{ width: '10%' }}>Step</SummaryTdHeader>
                   <SummaryTdHeader style={{ width: '10%' }}>Time</SummaryTdHeader>
+                  <SummaryTdHeader>CreatedAt</SummaryTdHeader>
                   <SummaryTdHeader style={{}}>Log</SummaryTdHeader>
                   <SummaryTdHeader style={{ width: '19%' }}>Attachments</SummaryTdHeader>
                 </SummaryTableRow>
               </SummaryTableHeader>
-              {session.interactions.items.map((interaction, i) =>
-                <SummaryTableBody>
+              {console.log(session.interactions.items.sort(timeConverter))}
+              {session.interactions.items.sort(timeConverter).map((interaction, i) =>
+                <SummaryTableBody key={i}>
                   <tr key={interaction.id} onClick={() => getSessionLogs(interaction.id)} style={{ cursor: "pointer" }}>
                     <SummaryTdBody color={interaction.step.color} style={{ border: "hidden" }}>{interaction.step.name_of_step.substring(0, 7)}</SummaryTdBody>
                     <SummaryTdBody color={interaction.step.color} style={{ border: "hidden" }}>{interaction.duration}s</SummaryTdBody>
+                    <SummaryTdBody color={interaction.step.color} style={{ border: "hidden" }}>{interaction.createdAt.substring(0, 19)}</SummaryTdBody>
                     <SummaryTdBody color={interaction.step.color} style={{ border: "hidden" }}>{interaction.log_content.substring(0, 50) + "..."}</SummaryTdBody>
                     <SummaryTdBody color={interaction.step.color} style={{ border: "hidden" }}>{interaction.attachments.items.length > 0 ? <Image color="#5567FD" size="medium" /> : "---"}</SummaryTdBody>
                     {console.log(interaction)}
