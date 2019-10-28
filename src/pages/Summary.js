273bb93afc95767977513ccfb906ff231d5d1dca
Kenny Tan <kennytanzxc1@gmail.com>
2019-10-28 10:14:51 -0400

    Modal

--- src/pages/Summary.js
+++ src/pages/Summary.js
@@ -2,27 +2,21 @@ import React, { useContext, useEffect, useState } from "react";
 import SummarySession from "../components/SummaryComponents/SummarySession/";
 import SummaryLog from "../components/SummaryComponents/SummaryLog/";
 import { CompassContext } from "../context/CompassPage/context";
+import { ReviewModalContext } from "../context/ReviewModal/context"
+import ReviewLog from "../components/ModalComponents/ReviewLog"
 
 const SummaryPage = (props) => {
   const { compass, session, interaction } = useContext(CompassContext);
+  const { showModal } = useContext(ReviewModalContext);
 
   return (
     <>
       {
         interaction.hasOwnProperty("id") ? (
-          <SummaryLog
-          // attachments={interaction.attachments}
-          // currentLog={interaction.log_content}
-          // interactId={interaction.id}
-          // compassID={compass.id}
-          // sessionID={session.id}
-          // interactionID={interaction.id}
-          // comments={interaction.comments}
-          />
-        ) : <SummarySession compassID={compass.id} />
+          < SummaryLog />
+        ) : <SummarySession />
       }
-      {/* <SummarySession compassID={compassID} /> */}
-
+      {showModal && <ReviewLog />}
     </>
   )
 };
