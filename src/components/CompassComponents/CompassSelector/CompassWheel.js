5fdf977716866a6e734e551eef0989229cb50cac
abraham villaroman <bvillaroman@gmail.com>
2019-10-27 23:37:03 -0400

    updated compass wheel to have better handling for selecting a step

--- src/components/CompassComponents/CompassSelector/CompassWheel.js
+++ src/components/CompassComponents/CompassSelector/CompassWheel.js
@@ -6,22 +6,32 @@ import {
   CompassWheelContainer,
   CompassWheel
 } from "../../../styles/CompassPage"
-// import * as Mutation from "../../../utils/mutations"
-import ReactApexChart from 'react-apexcharts';
+import * as Mutation from "../../../utils/mutations"
+// import ReactApexChart from 'react-apexcharts';
+import PieChart from 'react-minimal-pie-chart';
 
-export default ({ selectStep }) => {
-  const {session, interactions} = useContext(CompassContext)
+const FULL_WIDTH = 32;
+const NORMAL_WIDTH = 30;
+
+export default (props) => {
+  const {compass, session, interactions, updateInteraction, clearInteraction} = useContext(CompassContext)
   const [steps,setSteps] = useState([])
   const [options, setOptions]= useState({})
+  const [activeStep, setActiveStep] = useState({})
 
   useEffect(() => {
     if(session.hasOwnProperty("compass")){
       const compassSteps = session.compass.steps.items.flat();
-      let arr = compassSteps.map((step) => ({
+      let arr = compassSteps.map((step, key) => ({
           id : step.id,
+          key,
           name_of_step : step.name_of_step,
           color: step.color,
-          duration: 0
+          defaultColor: step.color,
+          duration: 0,
+          title: step.name_of_step,
+          value: 100/compassSteps.length
+
         })
       );
 
@@ -80,26 +90,104 @@ export default ({ selectStep }) => {
   }, [steps])
 
   const goToLog = async (id) => {
-    // Mutation.startInteraction(session.id,id)
-    // .then((res) => {
-    //   selectStep(res.data.createInteraction);   
-    // });
+    Mutation.startInteraction(session.id,id)
+    .then((res) => {
+      updateInteraction(res.data.createInteraction);   
+    });
   } 
 
+  function onMouseOut(event, propsData, index) {
+    const data = propsData.map((entry, i) => {
+      return i === index
+        ? {
+            ...entry,
+            style: {
+              ...entry.style,
+              strokeWidth: i === activeStep.key ? FULL_WIDTH : NORMAL_WIDTH,
+            },
+          }
+        : entry;
+    });
+
+    setSteps(data)
+  }
+
+  function onMouseOver(event, propsData, index) {
+    const data = propsData.map((entry, i) => {
+      return i === index
+        ? {
+            ...entry,
+            style: {
+              ...entry.style,
+              strokeWidth: FULL_WIDTH,
+            },
+          }
+        : entry;
+    });
+
+    setSteps(data)
+  }
+
+  function onClick(event, propsData, index) {
+    
+    const data = propsData.map((entry, i) => {
+      return {
+        ...entry,
+        ...{
+          style: {
+            ...entry.style,
+            strokeWidth: i === index && i !== activeStep.key ? FULL_WIDTH : NORMAL_WIDTH,
+            labelStyle:{
+              fill: 'black',
+              fontSize: '0.2rem'
+            }
+          },
+        },
+      };
+    });
+
+    setSteps(data)
+    if (activeStep.key !== index) {
+      setActiveStep(steps[index])
+      goToLog(steps[index].id)
+    }
+    else { 
+      setActiveStep({}) 
+      clearInteraction()
+    }
+  }
+
+  
   return (
     <CompassWheelContainer >
       <CSTitle gridArea="title">
         <span>Compass Steps</span>  
       </CSTitle>
-      <CompassWheel >
+      <CompassWheel color={activeStep.defaultColor}>
         {
           options.series && (
-            <ReactApexChart 
-              options={options} 
-              type="donut" 
-              series={options.series}
-              width="100%"
-              height="100%"
+            // <ReactApexChart 
+            //   options={options} 
+            //   type="donut" 
+            //   series={options.series}
+            //   width="100%"
+            //   height="110%"
+            // />
+            <PieChart
+              label={({ data, dataIndex }) => data[dataIndex].title }
+              labelPosition={70}
+              labelStyle={{
+                fill: 'white',
+                fontSize: '20%',
+                fontWeight: "550"
+              }}
+              lineWidth={60}
+              data={steps}
+              segmentsStyle={{ transition: 'stroke .3s' }}
+              onClick={onClick}
+              onMouseOver={onMouseOver}
+              onMouseOut={onMouseOut}
+              animate
             />
           )
         }
