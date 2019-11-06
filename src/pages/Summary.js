6a13ed97e8393b92686c2f7336f0cb7fc3cf7ac1
Kenny Tan <kennytanzxc1@gmail.com>
2019-11-06 13:23:39 -0500

    subscription iteraction 1.0

--- src/pages/Summary.js
+++ src/pages/Summary.js
@@ -2,12 +2,93 @@ import React, { useContext, useEffect, useState } from "react";
 import SummarySession from "../components/SummaryComponents/SummarySession/";
 import SummaryLog from "../components/SummaryComponents/SummaryLog/";
 import { CompassContext } from "../context/CompassPage/context";
-import { ReviewModalContext } from "../context/ReviewModal/context"
-import ReviewLog from "../components/ModalComponents/ReviewLog"
+import { ReviewModalContext } from "../context/ReviewModal/context";
+import ReviewLog from "../components/ModalComponents/ReviewLog";
+import { updateInteractionSub } from "../utils/subscriptions"
+import { updateCompass } from "../utils/mutations"
+import { Iteration } from "grommet-icons";
 
 const SummaryPage = (props) => {
-  const { compass, session, interaction } = useContext(CompassContext);
+  const { compass, session, interaction, update } = useContext(CompassContext);
   const { showModal } = useContext(ReviewModalContext);
+  const [list, setList] = useState([]);
+  const [updateLog, setUpdatedLog] = useState([]);
+
+  // useEffect(() => {
+  //   const subscriber = updateInteractionSubTest().subscribe({
+  //     next: res => {
+  //       console.log(res)
+  //     }
+  //   });
+
+  //   return () => subscriber.unsubscribe()
+  // }, [])
+
+  useEffect(() => {
+    const sub = updateInteractionSub().subscribe((updatedInteraction) => {
+      // console.log(updatedInteraction.value.data.onUpdateInteraction)
+      const newUpdatedInteraction = updatedInteraction.value.data.onUpdateInteraction
+      // console.log(newUpdatedInteraction)
+      setUpdatedLog(newUpdatedInteraction)
+    })
+
+    return () => {
+      sub.unsubscribe();
+    }
+  }, [])
+
+  useEffect(() => {
+    if (updateLog.hasOwnProperty("id")) {
+      console.log("UpdateLog: ", updateLog)
+      const updatedInteractionSessionID = updateLog.session.id
+      console.log("Session ID: ", updatedInteractionSessionID)
+
+
+      // const oldInteractions = compass.sessions.items find w (SessionID).interactions.items 
+      const allSession = compass.sessions.items
+      console.log(allSession)
+
+      const oldInteractions = compass.sessions.items.find((session) => session.id === updatedInteractionSessionID)
+      console.log("Old Interactions: ", typeof oldInteractions)
+
+      const newInteraction = oldInteractions.interactions.items.map((interaction) => {
+        if (updateLog.id === interaction.id) {
+          return updateLog
+        } else {
+          return interaction
+        }
+      })
+      console.log("new interaction: ", newInteraction)
+
+      // const oldSession = compass.sessions.items.find w (SessionID)
+      // console.log(compass)
+
+      const prevSessionIndex = compass.sessions.items.findIndex((session) =>
+        session.id === updatedInteractionSessionID
+      )
+      let oldSessionOrInteraction = compass.sessions.items[prevSessionIndex].interactions.items
+      console.log("pre assigning: ", compass.sessions.items)
+      compass.sessions.items[prevSessionIndex].interactions.items = newInteraction
+      console.log("post assinging: ", compass.sessions.items)
+
+
+      // const newSession = oldSession.interactions.items = newInteractions
+
+
+      // newSessions = compass.sessions.items.find(session => session.id === newSession.id) session = newSession
+      // const newCompass = {
+      //  ...compass,
+      //  sessions {
+      //    items : newSession
+      //  }
+      // }
+      updateCompass(compass)
+      // const newCompass = {
+      //   ...compass,
+      //   sessions
+      // }
+    }
+  }, [updateLog])
 
   return (
     <>
--- src/utils/subscriptions.js
+++ src/utils/subscriptions.js
@@ -4,18 +4,28 @@ import * as subscriptions from '../graphql/subscriptions';
 import config from '../aws-exports';
 API.configure(config);
 
-export const updateProjectsSub = () => API.graphql( graphqlOperation(subscriptions.onCreateCompass))
+export const updateProjectsSub = () => API.graphql(graphqlOperation(subscriptions.onCreateCompass))
 
-export const updateInteractionSub = async () => {
-  const subscription = await API.graphql( graphqlOperation(subscriptions.onUpdateInteraction))
-    
-  return subscription
-}
+// export const updateInteractionSub = async () => {
+//   const subscription = await API.graphql(graphqlOperation(subscriptions.onUpdateInteraction))
 
-export const createInteractionSub = async () => {
-  const subscription = await API.graphql( graphqlOperation(subscriptions.onCreateInteraction))
-    .subscribe({
-      next: (interactionData) => console.log(interactionData)
-    })
+//   return subscription
+// }
+
+// export const createInteractionSub = async () => {
+//   const subscription = await API.graphql(graphqlOperation(subscriptions.onCreateInteraction))
+//     .subscribe({
+//       next: (interactionData) => console.log(interactionData)
+//     })
+//   return subscription
+// }
+
+// export const updateInteractionSubTest = () => API.graphql(graphqlOperation(subscriptions.onUpdateInteraction))
+
+export const updateInteractionSub = () => {
+  const owner = localStorage.getItem("authuser");
+  console.log(owner);
+  const subscription = API.graphql({ query: subscriptions.onUpdateInteraction, variables: { owner: owner } })
+
   return subscription
 }
