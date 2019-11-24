1a6c6c67e2205010b09ed520c9eff95d7533544a
abraham villaroman <bvillaroman@gmail.com>
2019-11-23 20:47:26 -0500

    added back old compass

--- src/components/CompassComponents/CompassSelector/CompassSection.js
+++ src/components/CompassComponents/CompassSelector/CompassSection.js
@@ -3,13 +3,16 @@ import { CompassSectionContainer } from "../../../styles/CompassPage"
 import Loadable from "@loadable/component"
 
 import InteractionFeed from "./InteractionFeed"
-const CompassWheel = Loadable(() => import('./CompassWheel'));
+// const CompassWheel = Loadable(() => import('./CompassWheel'));
+const NewCompassWheel = Loadable(() => import('./NewCompassWheel'));
 
 const CompassSelector = ({selectStep}) => {
 
   return (
     <CompassSectionContainer>
-      <CompassWheel selectStep={selectStep} />
+      {/* <CompassWheel selectStep={selectStep} /> */}
+      <NewCompassWheel selectStep={selectStep} />
+      {/* <InteractionFeed /> */}
     </CompassSectionContainer>
 )};
 export default CompassSelector;
