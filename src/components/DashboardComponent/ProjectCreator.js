591941f83d2904ada9bcd6f064bfeacd9f7ea358
abraham villaroman <bvillaroman@gmail.com>
2019-12-15 15:48:25 -0500

    updated the styling for dashboard component

--- src/components/DashboardComponent/ProjectCreator.js
+++ src/components/DashboardComponent/ProjectCreator.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import styled from "styled-components"
 
-import { ProjectTypeFeed, ProjectTypeFeedContainer, DashboardSectionHeader } from "../../styles/Dashboard"
-
+import {DashboardSectionHeader} from "./style"
 import ProjectTemplate from "./ProjectTemplate"
 import CustomTemplate from "./CustomTemplate"
-export default (props) => {
 
+export const ProjectCreator = (props) => {
   return (
     <ProjectTypeFeedContainer>
       <DashboardSectionHeader>
@@ -18,3 +18,27 @@ export default (props) => {
     </ProjectTypeFeedContainer>
   )
 }
+
+export default ProjectCreator;
+
+const ProjectTypeFeedContainer = styled.div`
+  min-height: 15.6rem;
+  color: black;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  padding: 1rem 2rem; 
+  @media (max-width: 767px){
+    padding: 0;
+  }
+`;
+const ProjectTypeFeed = styled.div`
+  width: 100%;
+  min-height: 12rem;
+  color: black;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+`;
--- src/components/DashboardComponent/ProjectTemplate.js
+++ src/components/DashboardComponent/ProjectTemplate.js
@@ -1,14 +1,9 @@
 import React, { useContext } from 'react'
+import styled from "styled-components"
 import { GlobalContext } from "../../context/context"
-import {
-  TemplateContainer,
-  CompassCircle,
-  StepContainer,
-  CompassCardTitle,
-  ProjectTypeDetails
-} from "../../styles/Dashboard"
 import { createCompass, createStep } from "../../utils/mutations"
 import { navigate } from "gatsby"
+import {Link} from "gatsby"
 
 export default (props) => {
   const { user } = useContext(GlobalContext);
@@ -66,7 +61,7 @@ export default (props) => {
   return (
     <TemplateContainer onClick={goToReview}>
       <ProjectTypeDetails>
-        <CompassCircle gridArea="content" circleLength={defaultCompass.length}>
+        <CompassCircle circleLength={defaultCompass.length}>
           {
             defaultCompass.map((item, key) => (
                 <StepContainer
@@ -84,3 +79,56 @@ export default (props) => {
     </TemplateContainer>     
   )
 }
+export const TemplateContainer = styled.div`
+  padding: 0 1rem;
+  height: 11rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+`;
+export const ProjectTypeDetails = styled.div`
+  cursor: pointer;
+  height: 9rem;
+  width: 100%!important;
+  color: black;
+  font-weight: 600;
+  font-size: 1.5rem;
+  display: flex;
+  flex-direction: row;
+  background: white;
+  box-shadow: 0 1px 3px 0 #d2d4d6;
+  -webkit-transition: box-shadow 150ms ease;
+  transition: box-shadow 150ms ease;
+  
+`;
+export const CompassCardTitle = styled.div`
+  width: 100%;
+  height: 1rem;
+  color: black;
+  font-weight: 450;
+  font-size: 0.9rem;
+  text-align: center;
+  
+`;
+export const CompassCircle = styled(Link)`
+  min-width: 7.7rem;
+  min-height: 7.7rem; 
+  height: 100%;
+  position: relative;
+  margin: 0 auto;
+`
+export const StepContainer = styled.div`
+  border: 0.15rem solid ${props => props.color ? props.color : '#5567FD'};
+  border-radius: 50%;
+  transition: all 0.3s;
+  top: 40%;
+  left: 40%;
+  background-color: ${props => props.color ? props.color : '#5567FD'};
+  list-style: none;
+	height: 1.4rem;
+  width: 1.4rem;
+  display: block;
+	position: absolute;
+  transform: ${props => `rotate(${props.rotateAngle}deg) translate(${props.circleLength * 0.32}rem) rotate(-${props.rotateAngle}deg)` || 'none'};
+`
