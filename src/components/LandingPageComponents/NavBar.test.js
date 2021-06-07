8b7c7caba0c0a914d4e9f88c6f4afa8af94a3ca8
Kenny Tan <ktan@somethingdigital.com>
2021-06-06 21:56:36 -0400

    [LandingPage] Add unit tests and update export

---
+++ src/components/LandingPageComponents/NavBar.test.js
@@ -0,0 +1,34 @@
+import React from "react"
+import Enzyme, { shallow } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+import NavBar from "./NavBar"
+import {
+  NavContainer,
+  GatsbyLink,
+  LinkContainer,
+  LogoBox,
+} from "../../styles/LandingPage"
+
+Enzyme.configure({ adapter: new Adapter() })
+
+describe("<NavBar />", () => {
+  it("should contain NavBar content", () => {
+    const wrapper = shallow(<NavBar />)
+
+    const navContainer = wrapper.find(NavContainer)
+    expect(navContainer).toHaveLength(1)
+
+    const logoBox = wrapper.find(LogoBox)
+    expect(logoBox).toHaveLength(1)
+    expect(logoBox.contains(<GatsbyLink to="/" label="UDCompass" />)).toBe(true)
+
+    const linkContainer = wrapper.find(LinkContainer)
+    expect(linkContainer).toHaveLength(1)
+    expect(
+      linkContainer.contains(<GatsbyLink to="/" label="About UDC" />)
+    ).toBe(true)
+    expect(linkContainer.contains(<GatsbyLink to="/" label="Contact" />)).toBe(
+      true
+    )
+  })
+})
--- src/components/LandingPageComponents/index.js
+++ src/components/LandingPageComponents/index.js
@@ -1,12 +1,1 @@
-import React from "react";
-import NavBar from "./NavBar"
-import MainView from "./MainView"
-import { LandingPageContainer } from "../../styles/LandingPage"
-
-const LandingPage = (props) => (
-  <LandingPageContainer>
-    <NavBar />
-    <MainView />
-  </LandingPageContainer>
-);
-export default LandingPage;
+export { default as Landing } from "./LandingPage"
